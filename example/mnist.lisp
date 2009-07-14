422cde12af33b150b62210e5c282428227f15ac7
Gábor Melis <mega@retes.hu>
2009-07-14 18:40:24 +0200

    mnist/2: do it like ruslan's code

--- example/mnist.lisp
+++ example/mnist.lisp
@@ -65,6 +65,9 @@
 (defstruct image
   (label nil :type (integer 0 10))
   (array nil :type flt-vector))
+
+(defmethod label ((image image))
+  (image-label image))
 
 
 ;;;; Reading the files
@@ -141,22 +144,26 @@
 
 ;;;; Sampling, clamping, utilities
 
-(defun make-sampler (images &key max-n)
+(defun make-sampler (stories &key max-n omit-label-p)
   (make-instance 'counting-function-sampler
                  :max-n-samples max-n
-                 :sampler (make-random-generator images)))
+                 :sampler (let ((g (make-random-generator stories)))
+                            (lambda ()
+                              (list (funcall g) :omit-label-p omit-label-p)))))
 
 (defun clamp-array (image array start)
   (declare (type flt-vector array)
            (optimize (speed 3)))
   (replace array (image-array image) :start1 start))
 
-(defun clamp-striped-nodes (samples striped)
+(defun clamp-striped-nodes (images striped)
   (let ((nodes (storage (nodes striped))))
-    (loop for sample in samples
+    (loop for image in images
           for stripe upfrom 0
-          do (with-stripes ((stripe striped start))
-               (clamp-array sample nodes start)))))
+          do (destructuring-bind (image &key omit-label-p) image
+               (declare (ignore omit-label-p))
+               (with-stripes ((stripe striped start))
+                 (clamp-array image nodes start))))))
 
 (defun bias-cloud-p (cloud)
   (or (typep (chunk1 cloud) 'constant-chunk)
@@ -182,10 +189,10 @@
 (defclass mnist-logging-trainer (logging-trainer) ())
 
 (defmethod log-training-period ((trainer mnist-logging-trainer) learner)
-  10000)
+  1000)
 
 (defmethod log-test-period ((trainer mnist-logging-trainer) learner)
-  (length *training-images*))
+  10000 #+nil(length *training-images*))
 
 
 ;;;; DBN
@@ -211,6 +218,60 @@
              n-inputs)
     (reset-counter counter)))
 
+;;;;;;;;;;;;;;;
+
+(defgeneric label (obj))
+
+(defun count-misclassifications (examples striped
+                                 &key (label-fn #'label)
+                                 (stripe-label-fn #'stripe-label))
+  "Return the number of classification errors and the number of
+examples. The length of EXAMPLES must be equal to the number of
+stripes in STRIPED. LABEL-FN takes an example and returns its label
+that compared by EQL to what STRIPE-LABEL-FN returns for STRIPED and
+the index of the stripe."
+  (let ((n 0))
+    (loop for example in examples
+          for stripe upfrom 0
+          do  (unless (= (funcall label-fn example)
+                         (funcall stripe-label-fn striped stripe))
+                (incf n)))
+    (values n (length examples))))
+
+(defclass labeled () ()
+  (:documentation "Mixin for chunks/whatever that hold labels."))
+
+(defclass softmax-label-chunk (softmax-chunk labeled) ())
+
+(defgeneric maybe-make-misclassification-measurer (obj)
+  (:method (obj)
+    (values nil nil))
+  (:method ((labeled labeled))
+    (lambda (examples)
+      (count-misclassifications examples labeled))))
+
+(defun max-position (array start end)
+  (position (loop for i upfrom start below end maximizing (aref array i))
+            array :start start :end end))
+
+(defgeneric stripe-label (obj stripe)
+  (:method ((chunk softmax-label-chunk) stripe)
+    (with-stripes ((stripe chunk start end))
+      (- (max-position (storage (nodes chunk)) start end)
+         start))))
+
+(defun make-reconstruction-misclassification-counters-and-measurers
+    (dbn &key (rbm (last1 (rbms dbn))))
+  (loop for chunk in (apply #'append
+                            (mapcar #'chunks
+                                    (subseq (rbms dbn)
+                                            0 (1+ (position rbm (rbms dbn))))))
+        for measurer = (maybe-make-misclassification-measurer chunk)
+        when measurer
+        collect (cons (make-instance 'error-counter)
+                      measurer)))
+;;;;;;;;;;
+
 (defmethod log-test-error ((trainer mnist-rbm-trainer) (rbm mnist-rbm))
   (log-msg "DBN TEST RMSE: ~{~,5F~^, ~} (~D)~%"
            (map 'list
@@ -220,7 +281,27 @@
                                                      #+nil
                                                      (length *test-images*))
                                        (dbn rbm) :rbm rbm))
-           (n-inputs trainer)))
+           (n-inputs trainer))
+  (let ((counters-and-measurers
+         (make-reconstruction-misclassification-counters-and-measurers
+          (dbn rbm) :rbm rbm)))
+    (when counters-and-measurers
+      (let ((errors (map 'list
+                         #'get-error
+                         (dbn-mean-field-errors (make-sampler *test-images*
+                                                              :max-n 1000
+                                                              #+nil
+                                                              (length
+                                                               *test-images*)
+                                                              :omit-label-p t)
+                                                (dbn rbm) :rbm rbm
+                                                :counters-and-measurers
+                                                counters-and-measurers))))
+        (log-msg "DBN TEST CLASSIFICATION ACCURACY: ~{~,2F%~^, ~} (~D)~%"
+                 (mapcar (lambda (e)
+                           (* 100 (- 1 e)))
+                         errors)
+                 (n-inputs trainer))))))
 
 (defmethod negative-phase :around (batch trainer (rbm mnist-rbm))
   (call-next-method)
@@ -245,24 +326,31 @@
           (map-into (storage (weights cloud))
                     (lambda () (flt (* 0.1 (gaussian-random-1)))))))))
 
-(defun train-mnist-dbn (dbn)
+(defun train-mnist-dbn (dbn &key n-epochs n-gibbs learning-rate decay
+                        visible-sampling)
   (loop for rbm in (rbms dbn)
         for i upfrom 0 do
         (log-msg "Starting to train level ~S RBM in DBN.~%" i)
-        (train (make-sampler *training-images*
-                             :max-n (* 50 (length *training-images*)))
-               (make-instance 'mnist-rbm-trainer
-                              :segmenter
-                              (lambda (cloud)
-                                (make-instance 'mnist-rbm-segment-trainer
-                                               :learning-rate (flt 0.1)
-                                               :momentum (flt 0.9)
-                                               :weight-decay
-                                               (if (bias-cloud-p cloud)
-                                                   (flt 0)
-                                                   (flt 0.0002))
-                                               :batch-size 100)))
-               rbm)))
+        (flet ((this (x)
+                 (if (listp x)
+                     (elt x i)
+                     x)))
+          (train (make-sampler *training-images*
+                               :max-n (* n-epochs (length *training-images*)))
+                 (make-instance 'mnist-rbm-trainer
+                                :visible-sampling (this visible-sampling)
+                                :n-gibbs (this n-gibbs)
+                                :segmenter
+                                (lambda (cloud)
+                                  (make-instance 'mnist-rbm-segment-trainer
+                                                 :learning-rate
+                                                 (flt (this learning-rate))
+                                                 :weight-decay
+                                                 (if (bias-cloud-p cloud)
+                                                     (flt 0)
+                                                     (flt (this decay)))
+                                                 :batch-size 100)))
+                 rbm))))
 
 
 ;;;; BPN
@@ -295,21 +383,19 @@
     (loop for image in images
           for stripe upfrom 0
           do
-          (clamp-image-on-bpn bpn stripe image)
-          (with-stripes ((stripe inputs inputs-start)
-                         (stripe expectations expectations-start
-                                 expectations-end))
-            (clamp-array image inputs-nodes inputs-start)
-            (locally (declare (optimize (speed 3)))
-              (fill expectations-nodes #.(flt 0) :start expectations-start
-                    :end expectations-end))
-            (setf (aref expectations-nodes
-                        (+ expectations-start (image-label image)))
-                  #.(flt 1))))))
-
-(defun max-position (array start end)
-  (position (loop for i upfrom start below end maximizing (aref array i))
-            array :start start :end end))
+          (destructuring-bind (image &key omit-label-p) image
+            (declare (ignore omit-label-p))
+            (clamp-image-on-bpn bpn stripe image)
+            (with-stripes ((stripe inputs inputs-start)
+                           (stripe expectations expectations-start
+                                   expectations-end))
+              (clamp-array image inputs-nodes inputs-start)
+              (locally (declare (optimize (speed 3)))
+                (fill expectations-nodes #.(flt 0) :start expectations-start
+                      :end expectations-end))
+              (setf (aref expectations-nodes
+                          (+ expectations-start (image-label image)))
+                    #.(flt 1)))))))
 
 (defun bpn-decode-digit (bpn lump-name)
   (let* ((predictions (find-lump lump-name bpn :errorp t))
@@ -508,7 +594,8 @@
         (t
          (setq *dbn/1* (make-mnist-dbn/1))
          (init-mnist-dbn *dbn/1*)
-         (train-mnist-dbn *dbn/1*)
+         (train-mnist-dbn *dbn/1* :n-epochs 50 :learning-rate 0.1
+                          :decay 0.0002 :visible-sampling nil)
          (save-weights (merge-pathnames "mnist-1.dbn" *mnist-dir*) *dbn/1*)))
   (setq *bpn/1* (unroll-mnist-dbn/1 *dbn/1*))
   (train-mnist-bpn *bpn/1*)
@@ -517,6 +604,34 @@
 
 ;;;; Code for the DBN to DBM to BPN approach (paper [2])
 
+(defclass mnist-rbm/2 (mnist-rbm) ())
+
+(defun clamp-labels (images chunk)
+  (let ((nodes (storage (nodes chunk))))
+    (loop for image in images
+          for stripe upfrom 0
+          do (destructuring-bind (image &key omit-label-p) image
+               (unless omit-label-p
+                 (with-stripes ((stripe chunk start end))
+                   (fill nodes (flt 0) :start start :end end)
+                   (setf (aref nodes (+ start (image-label image)))
+                         #.(flt 1))))))))
+
+(defmethod set-input (images (rbm mnist-rbm/2))
+  (call-next-method)
+  (sample-visible rbm)
+  (let ((label-chunk (find-chunk 'label rbm)))
+    (when label-chunk
+      (clamp-labels images label-chunk))))
+
+(defclass softmax-label-chunk* (softmax-label-chunk) ())
+
+(defmethod maybe-make-misclassification-measurer ((chunk softmax-label-chunk*))
+  (let ((measurer (call-next-method)))
+    (when measurer
+      (lambda (examples)
+        (funcall measurer (mapcar #'first examples))))))
+
 (defclass mnist-dbm (dbm)
   ((layers :initform (list
                       (list (make-instance 'constant-chunk :name 'c0)
@@ -524,10 +639,15 @@
                                            :size (* 28 28)))
                       (list (make-instance 'constant-chunk :name 'c1)
                             (make-instance 'sigmoid-chunk :name 'f1
-                                           :size 500))
+                                           :size 500)
+                            (make-instance 'softmax-label-chunk* :name 'label
+                                           :size 10 :group-size 10))
                       (list (make-instance 'constant-chunk :name 'c2)
                             (make-instance 'sigmoid-chunk :name 'f2
-                                           :size 1000))))))
+                                           :size 1000))))
+   (clouds :initform '(:merge
+                       (:chunk1 c0 :chunk2 label :class nil)
+                       (:chunk1 inputs :chunk2 label :class nil)))))
 
 (defun make-mnist-dbm ()
   (make-instance 'mnist-dbm :max-n-stripes 100))
@@ -615,12 +735,12 @@
                                          :weight-decay
                                          (if (bias-cloud-p cloud)
                                              (flt 0)
-                                             (flt 0.0002))
+                                             (flt 0.000))
                                          :batch-size 100)))
          dbm))
 
 (defun make-mnist-dbn/2 (dbm)
-  (mgl-bm:dbm->dbn dbm :dbn-class 'mnist-dbn :rbm-class 'mnist-rbm
+  (mgl-bm:dbm->dbn dbm :dbn-class 'mnist-dbn :rbm-class 'mnist-rbm/2
                    :dbn-initargs '(:max-n-stripes 100)))
 
 (defun unroll-mnist-dbm (dbm)
@@ -674,6 +794,9 @@
     (setq *test-images* (load-test)))
   (flet ((train-dbn ()
            (init-mnist-dbn *dbn/2*)
+           (train-mnist-dbn *dbn/2* :n-epochs 2 :n-gibbs '(1 5)
+                            :learning-rate '(0.05 0.01)
+                            :decay 0.001 :visible-sampling t)
            (train-mnist-dbn *dbn/2*)
            (save-weights (merge-pathnames "mnist-2.dbn" *mnist-dir*)
                          *dbn/2*))
@@ -722,4 +845,26 @@
 
 (train-mnist/2 :load-dbm-p t)
 
+(defmethod negative-phase (batch (trainer bm-pcd-trainer) bm)
+  (check-no-self-connection bm)
+  (loop repeat (n-gibbs trainer) do
+        (dolist (chunk (elt (layers bm) 0))
+          (sample-chunk chunk))
+        (dolist (chunk (elt (layers bm) 2))
+          (sample-chunk chunk))
+        (dolist (chunk (elt (layers bm) 1))
+          (set-mean (list chunk) bm)
+          (sample-chunk chunk))
+        (dolist (chunk (elt (layers bm) 0))
+          (set-mean (list chunk) bm))
+        (dolist (chunk (elt (layers bm) 2))
+          (set-mean (list chunk) bm)))
+  (accumulate-negative-phase-statistics
+   trainer bm
+   ;; The number of persistent chains (or fantasy particles), that is,
+   ;; N-STRIPES of PERSISTENT-CHAINS is not necessarily the same as
+   ;; the batch size. Normalize so that positive and negative phase
+   ;; has the same weight.
+   :multiplier (/ (length batch)
+                  (n-stripes (persistent-chains trainer)))))
 |#
